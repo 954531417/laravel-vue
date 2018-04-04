949e298993eb91be3b238b7a394c1a476f5ccda3
myerp <954531417@qq.com>
2018-04-04 17:41:37 +0800

    init files

---
+++ resources/assets/js/components/hello/test1.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>test5</h1>
+    </div>
+</template>
+
+<script>
+    export default{
+        components : {
+        },
+        data(){
+            return {
+            }
+        },
+        methods : {
+        },
+        mounted(){
+        }
+    }
+</script>
---
+++ resources/assets/js/hello.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>Hello666</h1>
+        <hr>
+        <router-view>
+
+        </router-view>
+    </div>
+</template>
+
+
+<script>
+    export default{
+        components : {
+
+        },
+        data(){
+            return {
+
+            }
+        },
+        methods : {
+
+        },
+        mounted(){
+
+        }
+    }
+</script>
